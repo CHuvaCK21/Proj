23b014242be7768feec18d104ce9d7ffde30196e
andre <andre@ANDREY-PC>
2023-11-16 22:32:33 +0300

    Добавьте файлы проекта.

---
+++ Proj/Fn.h
@@ -0,0 +1,4 @@
+void TrToBin(unsigned int a); // Объявление функции в заголовочном файле Fn.h
+int CheckStr(char * a, char * b); //Объявление функции проверки строки
+void Table(int ch, char*);
+void cleanArr(char *a);
