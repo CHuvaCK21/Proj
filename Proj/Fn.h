e370b8e9d603f8caba06870eedc6db308fa14bcc
Морозов Андрей Алексеевич <andrey-trance@hotmail.com>
2023-11-18 03:41:41 +0300

    Translate to binary with table

--- Proj/Fn.h
+++ Proj/Fn.h
@@ -1,4 +1,4 @@
 void TrToBin(unsigned int a); // Объявление функции в заголовочном файле Fn.h
 int CheckStr(char * a, char * b); //Объявление функции проверки строки
-void Table(int ch, char*);
+void Table(unsigned int *count, unsigned int*count2);
 void cleanArr(char *a);
